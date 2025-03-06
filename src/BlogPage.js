a64f63ebd30fbb2d1a187f05129a909c561353f6
Álvaro Condorí <condorialvaro95@gmail.com>
2025-03-05 23:17:54 -0300

    useParams: rutas dinámicas

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,7 +1,27 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {blogdata} from "./blogdata";
 
 const BlogPage = () => {
-  return <h1>BlogPage</h1>;
+  return (
+    <>
+      <h1>BlogPage</h1>
+
+      <ul>
+        {blogdata.map((post) => (
+          <BlogLink post={post} />
+        ))}
+      </ul>
+    </>
+  );
 };
+
+function BlogLink({post}) {
+  return (
+    <li>
+      <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+    </li>
+  );
+}
 
 export {BlogPage};
